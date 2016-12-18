fafe63c841c93d9f59371b83e1548973e455564c
Andy Liang <aliang2344@gmail.com>
2016-12-17 20:56:17 -0500

    starting client proto

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,49 @@ use bangor::db::DBIO;
 use bangor::server::Server;
 use std::net::SocketAddr;
 
-fn main() {
 
-    let mut event_loop = Core::new().unwrap();
-    let handle = event_loop.handle();
-    
-    let future = Server::new(handle, &"127.0.0.1:9001".parse::<SocketAddr>().unwrap());
-    event_loop.run(future).unwrap();
-
-    // this won't actually run until the future resolves
+fn append() {
     let mut db = DBIO::new();
     let result = db.append("/tmp/test_bangor_file", "hello, world".as_bytes());
     println!("{:?}", result);
+}
+
+//for the server
+fn start() {
+    let mut event_loop = Core::new().unwrap();
+    let future = Server::new(&event_loop, &"127.0.0.1:9001".parse::<SocketAddr>().unwrap());
+    event_loop.run(future).unwrap();
+}
+
+
+// for the client
+//
+extern crate futures;
+use futures::future::Future;
+use tokio_core::net::TcpStream;
+use std::io::Read;
+
+fn client_connect() {
+    let mut event_loop = Core::new().unwrap();
+    let addr = &"127.0.0.1:9001".parse::<SocketAddr>().unwrap();
+    let handle = event_loop.handle();
+    let future = TcpStream::connect(&addr, &handle).map(|mut stream|{
+        let mut buf = Vec::new();
+        stream.read_to_end(&mut buf);
+        println!("{:?}", buf);
+    });
+    event_loop.run(future).unwrap();
+}
+
+use std::thread;
+
+fn main() {
+
+    let handle = thread::spawn(||start());
+
+    client_connect();
+
+    handle.join();
+
+
 }
--- src/server/mod.rs
+++ src/server/mod.rs
@@ -2,24 +2,28 @@ extern crate futures;
 
 use std::net::SocketAddr;
 use futures::Future;
-use futures::stream::{ForEach, Stream};
+use futures::stream::Stream;
 use tokio_core::io::{copy, Io};
-use tokio_core::net::{TcpListener, Incoming, TcpStream};
-use tokio_core::reactor::{Handle, Core};
+use tokio_core::net::TcpListener;
+use tokio_core::reactor::Core;
 use std::io::Error;
+use std::io::Read;
 
 pub struct Server;
 impl Server {
-    pub fn new(handle: Handle, addr: &SocketAddr) -> Box<Future<Item=(), Error=Error>> {
+    pub fn new(core: &Core, addr: &SocketAddr) -> Box<Future<Item=(), Error=Error>> {
+        let handle = core.handle();
         let socket = TcpListener::bind(&addr, &handle).unwrap();
 
         println!("listening on {:?}", addr);
 
         //more or less what tokio-core's echo example is r.n.
         let done = socket.incoming().for_each(move |(socket, addr)| {
-            let pair = futures::lazy(|| Ok(socket.split()));
-            let amt = pair.and_then(|(reader, writer)| copy(reader, writer));
-
+            let pair = futures::lazy(|| Ok(socket));
+            //let amt = pair.and_then(|(reader, writer)| copy(reader, writer));
+            let amt = pair.and_then(|reader| {
+              Self::place(reader)
+            });
             handle.spawn(amt.then(move |result| {
                 println!("wrote {:?} bytes to {}", result, addr);
                 Ok(())
@@ -29,5 +33,47 @@ impl Server {
         });
 
         Box::new(done)
+    }
+
+    pub fn place<A>(from: A) -> BangRequest<A> where A: Read {
+        BangRequest {
+            state: State::Reading{
+                a: from,
+                buf: Vec::new(),
+                pos: 0
+            }
+        }
+    }
+}
+
+enum State<A, T> {
+    Reading {
+        a: A,
+        buf: T,
+        pos: usize,
+    },
+    Empty,
+}
+
+pub struct BangRequest<A> where A: Read {
+    state: State<A, Vec<u8>>
+}
+
+use futures::{Async, Poll};
+
+impl <A> BangRequest<A> where A: Read {
+    fn parse_request(&mut self) {
+        match self.state {
+            _ => ()
+        };
+    }
+}
+
+impl <A> Future for BangRequest<A> where A: Read {
+    type Item = usize;
+    type Error = Error;
+
+    fn poll(&mut self) -> Poll<usize, Error> {
+        Ok(Async::Ready(4))
     }
 }
